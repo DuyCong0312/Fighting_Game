f2b0c137a10d402a94ad02a5c2be01defa4b706d
DuyCong0312 <duycongta2003@gmail.com>
2025-04-01 22:49:46 +0700

    .......nm

---
+++ Assets/Scripts/General/ContinueAnimationHeavyHurt.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueAnimationHeavyHurt : StateMachineBehaviour
+{
+    private Rigidbody2D rb;
+    private Transform playerTransform;
+    private Transform touchGroundPos;
+    private CheckGround groundCheck;
+    private PlayerMovement playerMovement;
+    [SerializeField] private string nameAnimatorClip;
+    [SerializeField] private GameObject effectTouchGround;
+    [SerializeField] private float blowUpPower;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerTransform = animator.transform;
+        groundCheck = animator.GetComponent<CheckGround>();
+        rb = playerTransform.GetComponent<Rigidbody2D>();
+        playerMovement = playerTransform.GetComponent<PlayerMovement>();
+        BlowUp();
+        if(touchGroundPos == null)
+        {
+           touchGroundPos = playerTransform.Find("JumpPos");
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (groundCheck.isGround)
+        {
+            animator.Play(nameAnimatorClip);
+            Instantiate(effectTouchGround, touchGroundPos.position, touchGroundPos.rotation);
+            rb.velocity = Vector2.zero;
+        }
+    }
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+
+    private void BlowUp()
+    {
+        float direction = playerMovement.isFacingRight ? 1 : -1;
+        rb.velocity = new Vector2(direction * blowUpPower, blowUpPower / 2f);
+    }
+}
--- Assets/Scripts/General/PlayerHealth.cs
+++ Assets/Scripts/General/PlayerHealth.cs
@@ -7,9 +7,13 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float maxHealth = 100;
     public float currentHealth;
     [SerializeField] private HealthBar healthBar;
+    [SerializeField] private string animationHeavyHurtName;
 
     private Animator anim;
-
+    private float damageThreshold = 20f;  
+    private float damageTimeLimit = 2f; 
+    private float accumulatedDamage = 0f;
+    private float timeSinceLastDamage = 0f;
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
@@ -23,11 +27,22 @@ public class PlayerHealth : MonoBehaviour
         {
             TakeDamage(5);
         }
+        if (timeSinceLastDamage < damageTimeLimit)
+        {
+            timeSinceLastDamage += Time.deltaTime;
+        }
+        else
+        {
+            accumulatedDamage = 0;
+        }
+        PlayHeavyHurt();
     }
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         anim.SetTrigger("getHurt");
+        accumulatedDamage += damage;
+        timeSinceLastDamage = 0f;
         //doan duoi la day lui khi bi tan cong
         //this.transform.position = new Vector2(this.transform.position.x - 0.2f, this.transform.position.y);
         healthBar.UpdateHealthBar(currentHealth, maxHealth);
@@ -49,4 +64,12 @@ public class PlayerHealth : MonoBehaviour
         this.gameObject.SetActive(false);
     }
 
+    private void PlayHeavyHurt()
+    {
+        if(accumulatedDamage >= damageThreshold && timeSinceLastDamage < damageTimeLimit)
+        {
+            anim.Play(animationHeavyHurtName);
+            accumulatedDamage = 0f;
+        }
+    }
 }
